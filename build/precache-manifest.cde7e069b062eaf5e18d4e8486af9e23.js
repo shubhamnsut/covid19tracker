44bb334ce3a23da12565dad005557d6b73964c6f
shubhamnsut <58215319+shubhamnsut@users.noreply.github.com>
2021-11-09 19:03:20 +0530

    Add files via upload

---
+++ build/precache-manifest.cde7e069b062eaf5e18d4e8486af9e23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b60e47de4fb9b30a67a66c44e7f8f5ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed5874af5ddd9d27c8fd",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "a6c98e4df3beb5d61b68",
+    "url": "/static/css/main.dedeb5c0.chunk.css"
+  },
+  {
+    "revision": "ed5874af5ddd9d27c8fd",
+    "url": "/static/js/2.ad8e3933.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/static/js/2.ad8e3933.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6c98e4df3beb5d61b68",
+    "url": "/static/js/main.f4a33534.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
